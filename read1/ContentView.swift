ef26aed1f518d66c809df4d09a5e5a81c2cae15f
githup <waalruwaili22@twq.idserve.net>
2023-01-05 09:56:45 +0300

    1

--- read1/ContentView.swift
+++ read1/ContentView.swift
@@ -17,9 +17,9 @@ struct ContentView: View {
         
         
         
-        struct ContentView_Previews: PreviewProvider {
-            static var previews: some View {
+       // struct ContentView_Previews: PreviewProvider {
+       //     static var previews: some View {
                 ContentView()
             }
         }
-    }}
+   // }}
